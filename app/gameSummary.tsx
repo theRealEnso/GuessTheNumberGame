196f402d7c4a7e1372d0b119099b72051660bcdd
Ben <bennnnnnnvu@gmail.com>
2025-05-14 17:01:37 -0700

    finish styling basic styling for the game summary screen

--- app/gameSummary.tsx
+++ app/gameSummary.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, Image } from "react-native";
 import { useRouter } from "expo-router";
 
 // import context
@@ -8,7 +8,7 @@ import { NumberContext } from "@/context/numberContext";
 const GameSummary = () => {
     const router = useRouter();
 
-    const { reset } = useContext(NumberContext);
+    const { reset, guessCount, number } = useContext(NumberContext);
 
     // function to reset game
     const resetGame = () => {
@@ -24,15 +24,23 @@ const GameSummary = () => {
 
             {/* image */}
             <View style={styles.imageContainer}>
-
+                <Image 
+                    source={require("../assets/images/success.png")}
+                    style={styles.image}
+                >
+                </Image>
             </View>
 
             {/* game details */}
             <View style={styles.gameDetails}>
-                <Text></Text>
-                <Pressable onPress={resetGame}>
-                    <Text>Start new game</Text>
-                </Pressable>
+                <Text style={styles.text}>{`Your phone device needed ${guessCount} rounds`} </Text>
+                <Text style={styles.text}>{`to guess the number ${number}.`}</Text>
+                <View style={styles.pressable}>
+                    <Pressable onPress={resetGame}>
+                        <Text>Start new game</Text>
+                    </Pressable>
+                </View>
+
             </View>
         </View>
     )
@@ -42,37 +50,61 @@ export default GameSummary;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         alignItems: "center",
         justifyContent: "center",
     },
 
     labelContainer: {
-        flex: 1,
-        color: "#fff",
+        borderColor: "#fff",
         borderWidth: 2,
         paddingHorizontal: 16,
+        paddingVertical: 4,
         marginVertical: 100,
         alignItems: "center",
         justifyContent: "center",
-        maxHeight: 50,
+        // height: 100,
     },
 
     imageContainer: {
-        flex: 4,
+        justifyContent: "center",
+        alignItems: "center",
+        width: 300,
+        height: 300,
         borderWidth: 2,
-        borderRadius: 100,
+        borderRadius: 150,
+        borderColor: "black",
+        overflow: "hidden",
+    },
+
+    image: {
+        height: 300,
+        width: 300,
+        resizeMode: "cover",
     },
 
     gameDetails: {
-        flex: 4,
         alignItems: "center",
         justifyContent: "center",
+        maxWidth: "80%",
+        marginVertical: 50,
     },
 
     gameOverText: {
         color: "#fff",
-        fontSize: 16,
+        fontSize: 24,
+    },
+
+    text: {
+        color: "#fff",
+        fontSize: 18,
+    },
+
+    pressable: {
+        marginTop: 20,
+        backgroundColor: "#d1ab13",
+        borderRadius: 16,
+        paddingHorizontal: 12,
+        paddingVertical: 4,
     }
 });
 
